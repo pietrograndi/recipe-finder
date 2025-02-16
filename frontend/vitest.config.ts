2bce4c0fa3a676be07404e39eddaa7c5d4b37af7
pietro grandi <pietrograndi@gmail.com>
2025-02-16 10:08:04 +0100

    fix path alias config vitest

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,9 +1,15 @@
 // vitest.config.ts
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
