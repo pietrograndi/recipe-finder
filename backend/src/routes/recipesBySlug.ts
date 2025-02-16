671eeb5f3bda71764475394eb990e8087644d40b
pietro grandi <pietrograndi@gmail.com>
2025-02-16 22:01:36 +0100

    chore: update recipes endopoint

--- backend/src/routes/recipesBySlug.ts
+++ backend/src/routes/recipesBySlug.ts
@@ -8,10 +8,20 @@ export async function recipesBySlug(fastify: FastifyInstance) {
       const recipe = await db('recipes')
         .select('id', 'recipe_name as name', 'recipe_description as description', 'recipe_image as image')
         .where('recipe_name', slug).first();
+        
       if(!recipe) {
         return reply.status(404).send({ error: 'Recipe not found' });
       }
-      return recipe;
+
+      const ingredients = await db('recipe_ingredients')
+        .select('ingredient_id as id', 'ingredient_name as name', 'ingredient_description as description')
+        .join('ingredients', 'recipe_ingredients.ingredient_id', 'ingredients.id')
+        .where('recipe_id', recipe.id);
+
+      return {
+        ...recipe,
+        ingredients
+      };
     } catch (error) {
       request.log.error(error);
       return reply.status(500).send({ error: 'Internal Server Error' });
